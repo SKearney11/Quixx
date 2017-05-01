3d7106115031195866a39551118e5b5cfd10119e
Sean Kearney <seankearney@Seans-MacBook-Pro.local>
2017-05-01 16:37:31 +0100

    Initial Commit

---
+++ Quixx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Quixx
+//
+//  Created by Sean Kearney on 01/05/2017.
+//  Copyright © 2017 Sean Kearney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
